6528ce53ee9640379c2635236f4dbeeb51cf75be
judson <judsonleal.00@gmail.com>
2022-06-02 21:26:04 -0300

    Adc

--- math.js
+++ math.js
@@ -1,12 +1,16 @@
 function soma(a, b) {
+    return(4 + 2)
 }
 function subtracao(a, b) {
+    return(4 - 2)
 }
 
 function divisao(a, b) {
+    return(4 / 2)
 }
 
 function multiplicacao(a, b) {
+    return(4 * 2)
 }
 
 module.exports = {
